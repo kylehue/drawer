d595b21750d68bb5e8551deeadadf2c904691e15
kylehue <apeuda69@gmail.com>
2022-09-30 18:19:31 +0800

    update

--- src/treeview.js
+++ src/treeview.js
@@ -10,14 +10,9 @@ function getElement(element) {
 }
 
 class TreeView extends Directory {
-	constructor(treeObject = {}, options = {}) {
+	constructor(options = {}) {
+		super();
 		console.clear();
-		// Main tree object
-		this.treeObject = {
-			name: "treeview",
-			path: "/",
-			content: treeObject
-		}
 
 		// Options
 		this.options = Object.assign({
@@ -28,60 +23,9 @@ class TreeView extends Directory {
 			autoRefresh: false
 		}, options);
 
-		// Element where we'll put everything
 		this.element = getElement(this.options.element);
-
-		this.bind(this.treeObject);
 	}
 
-	scanContent(treeObject, callback) {
-		scanObject(treeObject, result => {
-			if (result.value.type) {
-				callback(result);
-			}
-		});
-	}
-
-	bind(treeObject) {
-		let res = {};
-		// this.scanContent(this.treeObject, result => {
-		// 	let value = result.value;
-		//
-		// 	console.log(result.value);
-		//
-		// 	//Initiate directory types as a class
-		// 	if (value.type == "directory") {
-		// 		let directory = new Directory({
-		// 			content: value.content,
-		// 			name: value.name
-		// 		});
-		//
-		// 	}
-		// });
-
-		scanObject(treeObject, result => {
-			if (result.value.type == "directory") {
-				let directory = new Directory({
-					name: result.name,
-					content: result.value.content,
-					parent: result.parent,
-					path: result.path
-				});
-
-
-
-			}
-			console.log("------------------------");
-			console.log("PATH: " + result.path);
-			console.log("PARENT:");
-			console.log(result.parent);
-			console.log("CHILD:");
-			console.log(result.value);
-			console.log("------------------------");
-		});
-
-		console.log(res);
-	}
 }
 
 module.exports = TreeView;
