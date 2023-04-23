273502df60ab5b4a625c901c193d76091e925ad9
kylehue <apeuda69@gmail.com>
2023-04-23 08:48:52 +0800

    feat: rename, delete, widget

--- src/File.ts
+++ src/File.ts
@@ -1,16 +1,37 @@
-import Drawer, { IDrawerOptions } from "./Drawer.js";
+import path from "path-browserify";
+import Drawer from "./Drawer.js";
+import FileWidget from "./FileWidget.js";
 import Folder from "./Folder.js";
+import { IDrawerOptions } from "./types.js";
 
 export default class File {
    public type: "file" = "file";
-   protected _domNode: HTMLDivElement = document.createElement("div");
+   public widget: FileWidget;
    constructor(
+      public drawer: Drawer,
       public parent: Folder,
-      public source: string,
-      private _options: IDrawerOptions
+      public source: string
    ) {
-      
+      this.widget = new FileWidget(this);
    }
 
-   test() {}
+   delete() {
+      this.drawer.items.delete(this.source);
+      this.widget.dispose();
+   }
+
+   rename(name: string) {
+      let dirname = path.dirname(this.source);
+      let newSource = path.join(dirname, name);
+      let mapItem = this.drawer.items.get(this.source);
+
+      if (mapItem) {
+         this.drawer.items.set(newSource, mapItem);
+         this.drawer.items.delete(this.source);
+         this.source = newSource;
+         this.widget.domNodes.input.value = name;
+      } else {
+         console.error(`Can't rename ${this.source}`);
+      }
+   }
 }
