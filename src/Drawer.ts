5d89224d05c3f6b44d3783a282526354de9a7d61
kylehue <apeuda69@gmail.com>
2023-04-23 08:55:45 +0800

    refactor: delegate folder instead of inheriting

--- src/Drawer.ts
+++ src/Drawer.ts
@@ -1,20 +1,21 @@
 import Folder from "./Folder.js";
+import File from "./File.js";
+import DrawerHooks from "./DrawerHooks.js";
 import FolderWidget from "./FolderWidget.js";
+import { IDrawerOptions, ItemTypeMap } from "./types.js";
 
-export interface IDrawerOptions {
-   element: HTMLElement;
-   folderClassName: string;
-   folderIcon: string | Node;
-   folderIconOpen: string | Node;
-   folderIconChevron: string | Node;
-   fileClassName: string;
-   fileIcon:
-      | string
-      | Node
-      | ((fileExtension: string) => string | Node);
-}
+export default class Drawer
+   extends DrawerHooks
+   implements Pick<Folder, "add" | "delete" | "clear" | "get">
+{
+   public options: IDrawerOptions;
+   public root: Folder;
+   public items: Map<string, Folder | File> = new Map();
 
-export default class Drawer extends Folder {
+   /**
+    *
+    * @param options Options for configuring the behavior and appearance of a drawer.
+    */
    constructor(options?: Partial<IDrawerOptions>) {
       // Options defaults
       let _options = Object.assign<IDrawerOptions, Partial<IDrawerOptions>>(
@@ -26,16 +27,42 @@ export default class Drawer extends Folder {
             folderIconChevron: "",
             fileClassName: "",
             fileIcon: "",
+            editFolderNameOnDoubleClick: false,
+            editFileNameOnDoubleClick: true,
          },
          options || {}
       );
 
-      super(null, "/", _options);
+      super();
+
+      this.options = _options;
+      this.root = new Folder(this, null, "/");
 
       //this.options.element.append(this.);
    }
 
+   add<K extends keyof ItemTypeMap>(source: string, type?: K | undefined) {
+      return this.root.add(source, type);
+   }
+
+   delete(source?: string) {
+      this.root.delete(source);
+   }
+
+   clear() {
+      this.root.clear();
+   }
+   
+   get<K extends keyof ItemTypeMap>(source: string, type?: K | undefined) {
+      return this.root.get(source, type);
+   }
+
+   /**
+    * Appends the root widget container to the specified HTML element.
+    *
+    * @param element The HTML element to which the root widget container will be appended.
+    */
    appendTo(element: HTMLElement) {
-      //element.append(this._domNode);
+      element.append(this.root.widget.domNodes.container);
    }
 }
